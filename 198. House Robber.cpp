4c0ab21d25a8ee511dd08ba309fc9cc2e6c12bb7
2pieare <91305830+shivam-kush@users.noreply.github.com>
2022-01-25 22:36:34 +0530

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        
+        int prev2 = nums[0], prev = max(nums[0],nums[1]), curr = prev;
+        
+        for(int i=2; i<n; i++){
+            curr = max(prev, prev2 + nums[i]);
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
+    }
+};
