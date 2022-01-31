0e8b7e9f35893cdedf7c2630c7175ac2ce38aa02
2pieare <91305830+shivam-kush@users.noreply.github.com>
2022-02-01 00:43:26 +0530

    dp

---
+++ 790. Domino and Tromino Tiling.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTilings(int n) {
+        const int MOD = 1e9 + 7;
+	      std::vector<long> dp = { 0, 1, 2, 5 };
+	      dp.resize (n+1);
+
+	      for (int i = 4; i <= n; i++) {
+		      dp[i]  = 2 * dp[i - 1] + dp[i - 3];
+		      dp[i] %= MOD;
+	      }
+
+	  return dp[n];
+    }
+}; 
