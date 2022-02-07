90da7fa37c83f7acdf7357cc3a0a777ecb142d39
2pieare <91305830+shivam-kush@users.noreply.github.com>
2022-02-07 12:17:58 +0530

    Create 1266. Minimum Time Visiting All Points.cpp

---
+++ 1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int time = 0;
+        if(points.size() == 1) return 0;
+        
+        for(int i=1; i<points.size(); i++){
+            time += max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+        }
+        return time;
+    }
+};
