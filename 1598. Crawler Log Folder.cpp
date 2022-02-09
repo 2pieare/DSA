7583f389944757d8449c43239284a0667022da97
2pieare <91305830+shivam-kush@users.noreply.github.com>
2022-02-09 11:29:39 +0530

    Create 1598. Crawler Log Folder.cpp

---
+++ 1598. Crawler Log Folder.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans = 0;
+        for(auto log : logs){
+                if(log[0] == '.' && log[1] == '.'){
+                    if(ans == 0) ans = 0;
+                    else ans--;
+                }else if(log[0] == '.' && log[1] == '/'){
+                    continue;
+                }else{
+                    ans++;
+                }
+        }
+        return ans;
+    }
+};
